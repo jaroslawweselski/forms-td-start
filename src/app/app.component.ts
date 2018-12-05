ae7b31f53be28d2cb7831a898311d13db4ffa469
Jarosław Weselski <jweselski@objectivity.co.uk>
2018-12-05 12:06:49 +0100

    Modify forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') form: NgForm;
+  defaultQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.form.form.patchValue({
+        userData: {
+          username: suggestedName
+        }
+    });
+  }
+
+  onSubmit() {
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secretQuestion;
+    this.user.answer = this.form.value.answer;
+    this.user.gender = this.form.value.gender;
+
+    this.submitted = true;
+
+    this.form.reset();
   }
 }
